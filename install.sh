af0180f1f6c3e8e24b2ba33aa48948809ef593c8
dsnode <miken@dialoguesolutions.net>
2018-02-27 10:46:23 -0500

    small fixes to isntall.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-
+#curl -sSL https://raw.githubusercontent.com/DialogueSolutions/Geth-ETC-DockerContainer/master/install.sh | bash
 echo -e '\e[92m###################################################################################################################################################'
 echo -e 'This script is intended on helping you setup and run a GETH ETC Node in a Docker container '
 echo -e 'This will allow you to easily and seperatly run multiple instances of the GETH ETC Node on one machine with specific ports'
 echo -e '###################################################################################################################################################\e[0m'
-echo ''
+echo
 echo -e '\e[92m###################################################################################################################################################'
 echo -e 'Are we installing Docker?:yN'
 echo -e '###################################################################################################################################################\e[0m'
-read installingDocker
-echo ''
-if [ $installingDocker = 'y' ]; then
+read installingDocker;
+echo
+if [[ $installingDocker == 'y' ]]; then
 	echo -e '\e[92m###################################################################################################################################################'
 	echo 'Installing Docker'
 	echo -e '#################################################\e[0m'
@@ -19,29 +19,30 @@ if [ $installingDocker = 'y' ]; then
 	sudo apt-get update &&
 	apt-cache policy docker-ce &&
 	sudo apt-get install -y docker-ce make &&
-	echo ''
+	echo
 	echo -e '\e[92m###################################################################################################################################################'
 	echo 'Allow current user to use Docker without "sudo"'
 	echo 'SSH Session restart will be needed'
 	echo -e '###################################################################################################################################################\e[0m'
 	sudo usermod -aG docker ${USER}
-	echo ''	
+	echo
 	echo -e '\e[92m###################################################################################################################################################'
 	echo 'Docker CE succesfully installed'
 	echo -e '###################################################################################################################################################\e[0m'
 else
-	echo ''
+	echo
 	echo -e '\e[92m###################################################################################################################################################'
 	echo 'Ommiting Docker Install'
 	echo -e '###################################################################################################################################################\e[0m'
-	echo ''
-fi	
+	echo
+fi
+
 echo -e '\e[92m###################################################################################################################################################'
 echo 'Would you like to setup GETH ETC Node container from Docker HUB Image or build from Dockerfile?'
 echo 'Chosing "d" will build from dockerfile, chosing "i" will build from Docker Hub Image(Much Faster)'
 echo -e '###################################################################################################################################################\e[0m'
 read instType	
-if [ $instType = 'd' ]; then
+if [[ $instType == 'd' ]]; then
     read -p 'Enter name for docker image: ' imageName
 	sudo docker build -t ${imageName,,} .
 else
@@ -51,24 +52,24 @@ echo -e '\e[92m#################################################################
 echo 'Would you like to run the container now? "yN"'
 echo -e '###################################################################################################################################################\e[0m'
 read runContainer
-if [ $runContainer = 'y' ]; then
+if [[ $runContainer == 'y' ]]; then
 	echo -e '\e[92m###################################################################################################################################################'
 	echo 'Type in the name for your container'
 	echo -e '###################################################################################################################################################\e[0m'
 	read -p 'Container Name: ' containerName
-	echo ''
+	echo
 	echo -e '\e[92m###################################################################################################################################################'
 	echo 'Select port on which GETH will listen for peers, port should be 4 - 5 digits long'
 	echo '1-65535 are available, and ports in range 1-1023 are the privileged ones so dont use them'
 	echo -e '###################################################################################################################################################\e[0m'
 	read -p 'Container GETH Port: ' gethPort
-	echo ''
+	echo 
 	echo -e '\e[92m###################################################################################################################################################'
 	echo 'Select port on which GETH will listen for RPC connections, port should be 4 - 5 digits long'
 	echo '1-65535 are available, and ports in range 1-1023 are the privileged ones so dont use them'
 	echo -e '###################################################################################################################################################\e[0m'
 	read -p 'Container GETH RPC Port: ' rpcPort
-	echo ''
+	echo 
 	echo -e '\e[92m###################################################################################################################################################'
 	echo 'Creating directorioes and startGeth.sh file'
 	echo -e '###################################################################################################################################################\e[0m'
@@ -81,12 +82,12 @@ if [ $runContainer = 'y' ]; then
 	echo '###############################################################################################' >> $HOME/.ethereum-classic/$containerName/startGeth.sh &&
 	echo '' >> $HOME/.ethereum-classic/$containerName/startGeth.sh &&
 	echo '#Start Geth' >> $HOME/.ethereum-classic/$containerName/startGeth.sh &&
-	echo 'geth --sputnikvm --fast --identity='$containerName' --rpc --maxpeers=55 --verbosity=6' >> $HOME/.ethereum-classic/$containerName/startGeth.sh &&
-	echo ''
+	echo 'geth --sputnikvm --fast --identity='$containerName' --rpc --maxpeers=55 --cache=2014 --verbosity=6' >> $HOME/.ethereum-classic/$containerName/startGeth.sh &&
+	echo
 	echo -e '\e[92m#################################################'
 	echo 'Starting Container'
 	echo -e '#################################################\e[0m'
-	if [ $instType = 'd' ]; then
+	if [[ $instType == 'd' ]]; then
 		sudo docker run -tid --name $containerName -p $gethPort:30303/tcp -p $gethPort:30303/udp -p $rpcPort:8545/tcp --mount type=bind,source=$HOME/.ethereum-classic/$containerName,target=/.ethereum-classic/ ${imageName,,}
 	else
 		sudo docker run -tid --name $containerName -p $gethPort:30303/tcp -p $gethPort:30303/udp -p $rpcPort:8545/tcp --mount type=bind,source=$HOME/.ethereum-classic/$containerName,target=/.ethereum-classic/ bakon3/etcnode:v.08
@@ -103,3 +104,4 @@ else
 	echo 'If you decide to run the conatiner at a later time just run this installation script again.'
 	echo -e '###################################################################################################################################################\e[0m'
 fi
+
