cea6af3b264b2e76b55e7c625a2b63c48eb75455
Ethernode.io <38053571+ethernodeio@users.noreply.github.com>
2018-04-24 10:43:49 -0500

    No Docker Install for GETH-etc and SputnikVM compiled from source.
    Pull/Compile/Install GETH-etc and SputnikVM from source with one install script with out docker.

---
+++ nodockerInstall.sh
@@ -0,0 +1,77 @@
+!#/bin/bash
+
+#Update everything before isntalling any extra repositories or packages
+apt-get update -y &&
+apt-get upgrade -y &&
+apt-get install -y build-essential &&
+apt-get install -y git wget curl nano redis-server nginx software-properties-common python-software-properties rustc cargo apt-transport-https &&
+
+#Add mongoDB pbk
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5 &&
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.6.list &&
+apt-get update -y &&
+apt-get install -y mongodb-org &&
+apt-get install -y mongodb &&
+
+#Install latest nodejs
+curl -sL https://deb.nodesource.com/setup_9.x | bash - &&
+apt-get install -y nodejs &&
+
+#Install latest golang on Ubuntu
+add-apt-repository -y ppa:longsleep/golang-backports &&
+apt-get update -y &&
+apt-get install -y golang-go &&
+export GOPATH=$HOME/go &&
+
+#Download latest version of explorer
+cd /.ethereum-classic &&
+git clone https://github.com/ethernodeio/explorer.git &&
+cd explorer
+npm install &&
+npm install pm2 -g &&
+
+Download GETH Source
+echo -e '\e[92m#################################################'
+echo 'Download Latest GETH version from source'
+echo -e '#################################################\e[0m'
+cd $HOME/ &&
+go get -v github.com/ethereumproject/go-ethereum/... &&
+
+#Download sputnikvm build files
+echo -e '\e[92m#################################################'
+echo 'Download Latest sputnikVM version from source'
+echo -e '#################################################\e[0m'
+cd $HOME/go/src/github.com/ethereumproject/ &&
+git clone https://github.com/ethereumproject/sputnikvm-ffi &&
+cd sputnikvm-ffi/c/ffi &&
+
+#Build sputnikvm
+echo -e '\e[92m#################################################'
+echo 'Building sputnikVM'
+echo -e '#################################################\e[0m'
+cargo build --release &&
+cp $HOME/go/src/github.com/ethereumproject/sputnikvm-ffi/c/ffi/target/release/libsputnikvm_ffi.a $HOME/go/src/github.com/ethereumproject/sputnikvm-ffi/c/libsputnikvm.a &&
+
+#Build geth with sputnikvm
+echo -e '\e[92m#################################################'
+echo 'Building GETH with SputnikVM'
+echo -e '#################################################\e[0m'
+cd $HOME/go/src/github.com/ethereumproject/go-ethereum/cmd/geth &&
+CGO_LDFLAGS="$HOME/go/src/github.com/ethereumproject/sputnikvm-ffi/c/libsputnikvm.a -ldl" go build -tags=sputnikvm . &&
+
+#copy geth runtime to bin folder
+echo -e '\e[92m#################################################'
+echo 'Copying GETH to bin'
+echo -e '#################################################\e[0m'
+cp geth /usr/local/bin/ &&
+
+#Cloning Netstats API
+echo -e '\e[92m#################################################'
+echo 'Cloning Netstats API and installing PM2'
+echo -e '#################################################\e[0m'
+#install latest PM2 and etc-net-intelligence-api
+cd $HOME &&
+git clone https://github.com/ethernodeio/etc-net-intelligence-api.git etc-net-intelligence-api &&
+cd etc-net-intelligence-api &&
+npm install &&
+npm install pm2 -g
